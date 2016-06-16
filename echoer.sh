e39cb1b4314a73a54efd27b3824207b6ec4866b2
Tangierz <m.wierz@gmail.com>
2016-06-16 15:44:48 -0600

    Updated with another line print to stdout - fixed a parenthesis issue from the first.

--- echoer.sh
+++ echoer.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
 # Script that outputs a line of text
-echo "This is a test line of text
+echo "This is a test line of text"
+
+echo "And another thing - this is a mod on top of a pull request"
